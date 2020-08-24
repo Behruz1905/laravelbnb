06ed6ffe6e34c9a403666c27dade6886d5e70c88
User <behruznakhchivanski@gmail.com>
2020-08-25 00:22:54 +0400

    ILk kommit

---
+++ resources/js/bookables/Bookables.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <bookable-list-item item-title="Cheap Villa" item-content="A very Cheap Villa" v-bind:price="1000"></bookable-list-item>
+        <bookable-list-item item-title="Cheap Villa 2" item-content="A very Cheap Villa2" v-bind:price="2500"></bookable-list-item>
+  </div>
+</template>
+
+<script>
+import BookableListItem from "./BookableListItem";
+export default {
+    components: {
+        BookableListItem: BookableListItem
+    }
+}
+</script>
+
+<style>
+
+</style>template
+
---
+++ resources/js/routes.js
@@ -0,0 +1,27 @@
+import   VueRouter from "vue-router";
+import Bookables  from "./bookables/Bookables";
+import Example2 from "./components/Example2";
+
+
+const routes = [
+    {
+        path: "/",
+        component : Bookables,
+        name: "home",
+    },
+    {
+        path: "/second",
+        component : Example2,
+        name: "second",
+    },
+
+];
+
+
+const router = new VueRouter({
+    routes,
+    mode: "history"
+  })
+
+export default router;
+
