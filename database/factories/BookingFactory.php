76c56a5e064b62c33c75eaa583a1b5065085cdc4
Behruz <behruznakhchivanski@gmail.com>
2020-08-27 09:26:15 +0400

    resourcecontroller and invoke controller added 2

---
+++ database/factories/BookingFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Faker\Generator as Faker;
+use App\Model;
+use App\Booking;
+
+$factory->define(Booking::class, function (Faker $faker) {
+    $from = \Carbon\Carbon::instance($faker->dateTimeBetween('-1 months', '+1 months'));
+    $to = (clone $from)->addDays(random_int(0, 14));
+    return [
+        'from' => $from,
+        'to' => $to
+    ];
+});
+
---
+++ database/seeds/BookableTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Bookable;
+use Illuminate\Database\Seeder;
+
+class BookableTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Bookable::class, 100)->create()->each(function ($user) {
+           // $user->posts()->save(factory(App\Post::class)->make());
+        });
+    }
+}
